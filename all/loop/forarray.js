9991a29674bc235d1fa6817a2f17e54cf49f61b7
opu34 <nurmdopu428@gmail.com>
2021-08-02 20:13:16 +0600

    forloop

--- all/loop/forarray.js
+++ all/loop/forarray.js
@@ -1,8 +1,16 @@
 
+
 // array te loop calanor system
 
-var roll = [ 20, 50,70,89,76,98];
+// var roll = [ 20, 50,70,89,76,98];
 
-for( var r =0; r <6; r++ ){
-  console.log( roll[r]);
+// for( var r =0; r <6; r++ ){
+//   console.log( roll[r]);
+// }
+
+// string diye array te for loop
+
+var name = [ "akhi", "opu", "hridoy", "rokon"];
+for( i = 0; i<=3; i++){
+    console.log(name[i]);
 }
