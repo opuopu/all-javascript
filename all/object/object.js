b5f52acfe1d12040f58f5c5a221af13f590aed55
opu34 <nurmdopu428@gmail.com>
2021-08-03 14:57:56 +0600

    object

---
+++ all/object/object.js
@@ -0,0 +1,42 @@
+// basic object structure
+var family ={
+
+    member: 30,
+    name: "opu",
+    pc:40,
+    laptopName: "dell"
+}
+// output of family.(name)
+console.log(family.name);
+// change object property by 3 way
+
+// way-1
+family.pc = 4000;
+
+// way-2
+var pcprice = "pc";
+family[pcprice]= 5000;
+
+// console.log(pcprice);
+// way-3
+// console.log(family.pc);
+
+
+var member ={
+Storage:100,
+name:"dell",
+age: "1year",
+
+
+}
+console.log(member);
+
+//  way-1
+
+member.name = "hp";
+// way-2
+member["name"] = "walton";
+// way-3 variable diye kora jay
+var chnageName = "name";
+member["chnageName"] = " samsung";
+console.log(member.chnageName);
